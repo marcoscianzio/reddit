3f1f8755a83acced28bb7b71399aaacb97f015c8
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-18 20:40:33 -0300

    Add cokies and cors

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,60 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
+import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+
+import redis from "redis";
+import session from "express-session";
+
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
 
 const main = async () => {
   await createConnection();
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.set("trust proxy", true);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: ["https://studio.apollographql.com"],
+    })
+  );
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      },
+      saveUninitialized: false,
+      secret: "wejwqequuropñgmbvccxsise",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [PostResolver, UserResolver],
       validate: false,
     }),
+    context: ({ req, res }): MyContext => ({ req, res }),
   });
 
   await apolloServer.start();
@@ -23,6 +62,7 @@ const main = async () => {
   apolloServer.applyMiddleware({
     path: "/graphql",
     app,
+    cors: false,
   });
 
   app.listen(4000, () => {
