566f42b6154d8a4eb9aab1355be44ad189af2e94
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-26 08:31:14 -0300

    Add things

---
+++ server/dist/constants.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FORGET_PASSWORD_PREFIX = void 0;
+exports.FORGET_PASSWORD_PREFIX = "forget-password:";
+//# sourceMappingURL=constants.js.map
