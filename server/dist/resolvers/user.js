42dd6c2b4512b62c244bd7611e04e9d7363e65b9
MarcosCianzio <marcoscianziogamero@gmail.com>
2021-11-25 22:05:33 -0300

    Add logout

--- server/dist/resolvers/user.js
+++ server/dist/resolvers/user.js
@@ -111,6 +111,16 @@ let UserResolver = class UserResolver {
         req.session.userId = user.id;
         return { user };
     }
+    logout({ req, res }) {
+        return new Promise((resolve) => req.session.destroy((err) => {
+            res.clearCookie("qid");
+            if (err) {
+                resolve(false);
+                return;
+            }
+            resolve(true);
+        }));
+    }
 };
 __decorate([
     (0, type_graphql_1.Query)(() => user_1.User, { nullable: true }),
@@ -135,6 +145,13 @@ __decorate([
     __metadata("design:paramtypes", [user_2.UsernamePassword, Object]),
     __metadata("design:returntype", Promise)
 ], UserResolver.prototype, "login", null);
+__decorate([
+    (0, type_graphql_1.Mutation)(() => Boolean),
+    __param(0, (0, type_graphql_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], UserResolver.prototype, "logout", null);
 UserResolver = __decorate([
     (0, type_graphql_1.Resolver)()
 ], UserResolver);
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -120,4 +120,19 @@ export class UserResolver {
 
     return { user };
   }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie("qid");
+        if (err) {
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      })
+    );
+  }
 }
